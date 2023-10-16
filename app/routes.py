135317a78a0699a657ffb35a78a92376adbd8b6d
Haoyu Li <1409870309@qq.com>
2023-10-16 20:40:32 +0800

    add generate pdf function

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 import sqlite3
 
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session, make_response
 from wtforms import SelectMultipleField
 from app.forms import Majorform
 from app.database import getCourses, getMajors, getUnits, process_units, process_duplicates, ifvalid, filter_non_core_units, organize_non_core_units
-
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from io import BytesIO
 def init_routes(app):
 
     @app.route('/')
@@ -92,3 +95,49 @@ def init_routes(app):
         return jsonify(response)
 
 
+    @app.route('/DownloadPDF', methods=['POST'])
+    def download_pdf():
+        plan = []
+        data = request.json
+        plan.clear()
+        plan.extend(data.get('studyplan', []))
+        response = make_response(generate_pdf(plan))
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'inline; filename=study_plan.pdf'
+
+        return response
+
+    def generate_pdf(studyplan):
+        buffer = BytesIO()
+        doc = SimpleDocTemplate(buffer, pagesize=letter)
+        elements = []
+
+        for semester, courses in enumerate(studyplan, start=1):
+            data = []
+            data.append([f"Semester {semester}"])
+
+            for course in courses:
+                data.append([course])
+
+            table = Table(data)
+
+
+            style = TableStyle([
+                ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+            ])
+
+            table.setStyle(style)
+            elements.append(table)
+
+
+        doc.build(elements)
+
+        pdf = buffer.getvalue()
+        buffer.close()
+
+        return pdf
