cb9fbd4db4ee2447a9d94e3a4bde5954892441ce
panyeunglee <132634865+panyeunglee@users.noreply.github.com>
2023-10-20 07:44:33 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from app import app
 from flask import Flask, render_template
+import os
 
 app.jinja_env.globals.update(enumerate=enumerate)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = os.environ.get('PORT', 5000)  # Use 5000 as a default port if PORT is not set
+    app.run(host='0.0.0.0', port=port)
+
+
+
