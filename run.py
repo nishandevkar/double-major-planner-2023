ccb2dd2f8f62569b969da45275a5c0394dca2543
panyeunglee <132634865+panyeunglee@users.noreply.github.com>
2023-10-19 15:55:25 +0800

    Update run.py
    Set 5000 as the default port

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from app import app
 import sqlite3
+import os
+
+
 
 app.run(debug=True)
 
@@ -19,5 +22,7 @@ def fetch_data_from_unit():
     
     return rows
 
+
 if __name__ == '__main__':
-    app.run()
+    port = os.environ.get('PORT', 5000)  # Use 5000 as a default port if PORT is not set
+    app.run(host='0.0.0.0', port=port)
