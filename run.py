03a3d57b0909d67e5a2f4e0a5d06b8eb51d7f153
Panyeung Lee <panyeunglee@Panyeungs-MBP.lan>
2023-10-20 09:18:29 +0800

    change back run.py

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ import os
 app.jinja_env.globals.update(enumerate=enumerate)
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)  # Use 5000 as a default port if PORT is not set
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, port=8000)
+
 
 
 
